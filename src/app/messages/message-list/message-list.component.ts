002482677e2ee4a247e141862d8f70ff1f522275
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2024-02-12 22:23:06 +0100

    add stepper

--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-message-list',
-  standalone: true,
-  imports: [],
   templateUrl: './message-list.component.html',
   styleUrl: './message-list.component.scss'
 })
