3f9ada3a22bf53735b23fda0fadce4bb76335aea
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2024-02-13 15:37:32 +0100

    add form for new customer

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,28 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomerCardComponent } from './customer-card/customer-card.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerNewComponent } from './customer-new/customer-new.component';
 
 // Material components
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     CustomerCardComponent,
     CustomerListComponent,
+    CustomerNewComponent,
   ],
   imports: [
     CommonModule,
     CustomersRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
 
     //Material
     MatButtonModule,
     MatCardModule,
     MatIconModule,
+    MatInputModule,
   ]
 })
 export class CustomersModule { }
