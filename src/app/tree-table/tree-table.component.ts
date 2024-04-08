e4a0cea3c36279c2d5087832f0d0e470d16f0835
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2024-04-08 19:09:22 +0200

    add converter

---
+++ src/app/tree-table/tree-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tree-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './tree-table.component.html',
+  styleUrl: './tree-table.component.scss'
+})
+export class TreeTableComponent {
+
+}
